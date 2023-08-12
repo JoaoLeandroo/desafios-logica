243346f69465261f19a4f07323e073c0d84bc79b
joaoleandro <new.joaoleandro@gmail.com>
2023-08-12 19:41:23 -0300

    Desafio 16, concluido.

---
+++ desafio 16/index.js
@@ -0,0 +1,8 @@
+// Tabuada: Escreva um programa que gere a tabuada de multiplicação para um número específico.
+
+const numeroTabuada = 8
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${numeroTabuada} = ${i * numeroTabuada}`)
+}
+
