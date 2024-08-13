d84058ff197cbeff72230a68962a0522db1b39a5
joaoleandro <new.joaoleandro@gmail.com>
2024-08-13 20:31:59 -0300

    Desafio 24

---
+++ desafio 24/index.js
@@ -0,0 +1,10 @@
+// Crie um triangulo retangulo utilizando ***
+let x = ""
+
+for(let i = 15; i >= 1; i--) {
+    x = ""
+    for(let j = 1; j < i; j++) {
+        x += "*" 
+    }
+    console.log(x)
+}
